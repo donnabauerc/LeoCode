ef267ea1428ec09eac87cca0e382bb03144756f8
donnabauerc <christiandonnabauer@gmail.com>
2020-06-16 15:18:37 +0200

    add endpoint, which allows fileUpload into the test directory

---
+++ testing-api/src/main/java/at/htl/endpoints/TestEndpoint.java
@@ -0,0 +1,97 @@
+package at.htl.endpoints;
+
+import io.quarkus.launcher.shaded.org.apache.commons.io.IOUtils;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+@Path("/upload")
+public class TestEndpoint {
+    private final String UPLOADED_FILE_PATH = "../src/test/java/at/htl/examples/";
+
+    @POST
+    @Consumes("multipart/form-data")
+    public Response uploadFile(MultipartFormDataInput input) {
+
+        String fileName = "";
+
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+        // depends on form eg. name="uploadedFile"
+        List<InputPart> inputParts = uploadForm.get("uploadedFile");
+
+        for (InputPart inputPart : inputParts) {
+            try {
+                MultivaluedMap<String, String> header = inputPart.getHeaders();
+                fileName = getFileName(header);
+
+                //convert the uploaded file to inputstream
+                InputStream inputStream = inputPart.getBody(InputStream.class, null);
+
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+
+                //constructs upload file path
+                fileName = UPLOADED_FILE_PATH + fileName;
+
+                saveFile(bytes, fileName);
+
+                System.out.println("Uploaded " + fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return Response.ok("Uploaded " + fileName).build();
+    }
+
+    /*
+     * header sample
+     * {
+     * 	Content-Type=[image/png],
+     * 	Content-Disposition=[form-data; name="file"; filename="filename.extension"]
+     * }
+     */
+
+    //get uploaded filename
+    private String getFileName(MultivaluedMap<String, String> header) {
+
+        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("filename"))) {
+                String[] name = filename.split("=");
+
+                String finalFileName = name[1].trim().replaceAll("\"", "");
+
+                return finalFileName;
+            }
+        }
+        return "unknown";
+    }
+
+    private void saveFile(byte[] content, String filename) throws IOException {
+
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileOutputStream fop = new FileOutputStream(file);
+
+        fop.write(content);
+        fop.flush();
+        fop.close();
+    }
+}
