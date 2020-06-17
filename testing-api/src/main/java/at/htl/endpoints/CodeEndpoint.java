56ce5ccc1b6fb73a79af5ca341d919e3796918fa
donnabauerc <christiandonnabauer@gmail.com>
2020-06-17 13:50:15 +0200

    add Enpoint for Code & return Filename

--- testing-api/src/main/java/at/htl/endpoints/CodeEndpoint.java
+++ testing-api/src/main/java/at/htl/endpoints/CodeEndpoint.java
@@ -1,6 +1,5 @@
 package at.htl.endpoints;
 
-import io.quarkus.launcher.shaded.org.apache.commons.io.IOUtils;
 import io.quarkus.launcher.shaded.org.slf4j.Logger;
 import io.quarkus.launcher.shaded.org.slf4j.LoggerFactory;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
@@ -9,33 +8,27 @@ import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
-@Path("/test")
-public class TestEndpoint {
+@Path("/code")
+public class CodeEndpoint {
 
-    private final Logger log = LoggerFactory.getLogger(TestEndpoint.class);
+    private final Logger log = LoggerFactory.getLogger(CodeEndpoint.class);
 
     @POST
     @Path("/upload")
     @Consumes("multipart/form-data")
     public Response uploadFile(MultipartFormDataInput input) {
+
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
         // depends on form eg. name="uploadedFile"
         List<InputPart> inputParts = uploadForm.get("uploadedFile");
 
-        String fileName = FileGenerator.uploadFile("../src/test/java/at/htl/examples/", inputParts);
+        String fileName = FileGenerator.uploadFile("../src/main/java/at/htl/examples/", inputParts);
         log.info("Uploaded " + fileName);
 
         return Response.ok("Uploaded "+fileName).build();
     }
-
-
 }
