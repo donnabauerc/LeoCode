96cf0e2aa91b2b6cd67f26fda4402dbe1267b3c4
donnabauerc <christiandonnabauer@gmail.com>
2020-06-29 11:03:49 +0200

    test project uploaded per fileupload

---
+++ prototypes/testing-api-java/project-under-test/run-tests.sh
@@ -0,0 +1,9 @@
+# Run tests and generate .xml reports
+mvn test
+
+# Convert .xml reports into .html report, but without the CSS or images
+mvn surefire-report:report-only
+
+# Put the CSS and images where they need to be without the rest of the
+# time-consuming stuff
+mvn site -DgenerateReports=false
