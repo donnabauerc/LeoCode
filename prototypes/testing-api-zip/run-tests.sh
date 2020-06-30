67f59f42837944792cf8ea5ddd67645da98546af
donnabauerc <christiandonnabauer@gmail.com>
2020-06-30 22:49:21 +0200

    log only relevant output

--- prototypes/testing-api-zip/run-tests.sh
+++ prototypes/testing-api-zip/run-tests.sh
@@ -1,19 +1,20 @@
 PROJECT_NAME=$(ls ../project-under-test)
 
-cd ../project-under-test/$PROJECT_NAME
+cd ../project-under-test/code-examples
 # Run tests and generate .xml reports
-mvn test > ./log.txt
+mvn test
 
 # Convert .xml reports into .html report, but without the CSS or images
-mvn surefire-report:report-only >> ./log.txt
+mvn surefire-report:report-only
 
 # Put the CSS and images where they need to be without the rest of the
 # time-consuming stuff
-mvn site -DgenerateReports=false >> ./log.txt
+mvn site -DgenerateReports=false
 
 cd ..
 
 mv ./$PROJECT_NAME/target/site ../target/
-mv ./$PROJECT_NAME/log.txt ../target/
+
+cat ./$PROJECT_NAME/target/surefire-reports/*.txt > ../target/log.txt
 
 rm -rf ./$PROJECT_NAME
