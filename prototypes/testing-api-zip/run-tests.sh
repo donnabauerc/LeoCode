1ef2d3271152db75a0d55867dd0a259b1a1ccd7a
donnabauerc <christiandonnabauer@gmail.com>
2020-06-30 22:16:07 +0200

    log test output into file

--- prototypes/testing-api-zip/run-tests.sh
+++ prototypes/testing-api-zip/run-tests.sh
@@ -2,17 +2,18 @@ PROJECT_NAME=$(ls ../project-under-test)
 
 cd ../project-under-test/$PROJECT_NAME
 # Run tests and generate .xml reports
-mvn test
+mvn test > ./log.txt
 
 # Convert .xml reports into .html report, but without the CSS or images
-mvn surefire-report:report-only
+mvn surefire-report:report-only >> ./log.txt
 
 # Put the CSS and images where they need to be without the rest of the
 # time-consuming stuff
-mvn site -DgenerateReports=false
+mvn site -DgenerateReports=false >> ./log.txt
 
 cd ..
 
 mv ./$PROJECT_NAME/target/site ../target/
+mv ./$PROJECT_NAME/log.txt ../target/
 
 rm -rf ./$PROJECT_NAME
