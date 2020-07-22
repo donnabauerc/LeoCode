54c33be7d6bf23599a1d6d1b723b1aee0fc2b115
donnabauerc <christiandonnabauer@gmail.com>
2020-07-22 16:10:33 +0200

    add upload for teacher & reset function

---
+++ prototypes/testing-api-V3.0/src/main/java/at/htl/resources/UploadEndpoint.java
@@ -0,0 +1,49 @@
+package at.htl.resources;
+
+import at.htl.control.FileHandler;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logmanager.Logger;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
+
+@Path("upload")
+public class UploadEndpoint {
+
+    private final Logger log = Logger.getLogger(UploadEndpoint.class.getSimpleName());
+
+    public final String fileSeparator = System.getProperty("file.separator");
+
+    public String path = "..";
+
+    @ConfigProperty(name = "project-under-test")
+    String projectUnderTest;
+
+    public UploadEndpoint() {
+
+    }
+
+    @POST
+    @Path("/teacher")
+    @Consumes("multipart/form-data")
+    public Response uploadProject(MultipartFormDataInput input) {
+
+        path += fileSeparator + projectUnderTest + fileSeparator;
+
+        FileHandler.clearDirectory(path);
+
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+
+        uploadForm.forEach((k,v) -> {
+            FileHandler.uploadFile(path, v);
+        });
+
+        return Response.ok("Uploaded").build();
+    }
+}
