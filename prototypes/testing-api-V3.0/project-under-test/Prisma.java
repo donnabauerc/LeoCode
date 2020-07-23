645d7908c26cd8322703dee1bf9eaf6e915980b3
donnabauerc <christiandonnabauer@gmail.com>
2020-07-23 16:33:27 +0200

    remove unecessary files

--- prototypes/testing-api-V3.0/project-under-test/Prisma.java
+++
@@ -1,9 +0,0 @@
-package at.htl;
-
-public class Prisma {
-    public static void main(String[] args) {
-        System.out.println("=================");
-        System.out.println("║ Hello, World! ║");
-        System.out.println("=================");
-    }
-}
