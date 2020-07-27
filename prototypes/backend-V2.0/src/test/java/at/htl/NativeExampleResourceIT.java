406fd07d299d03ca6b9311c6508433485f44d871
donnabauerc <christiandonnabauer@gmail.com>
2020-07-27 22:55:40 +0200

    initial commit

---
+++ prototypes/backend-V2.0/src/test/java/at/htl/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package at.htl;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
