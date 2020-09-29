92c7a8320bfbb1edf7373ea18b17ba029ac861a0
donnabauerc <christiandonnabauer@gmail.com>
2020-09-29 13:13:52 +0200

    test configuration

--- production-system/testing-api/src/main/java/at/htl/resources/TestEndpoint.java
+++ production-system/testing-api/src/main/java/at/htl/resources/TestEndpoint.java
@@ -7,7 +7,9 @@ import org.jboss.logmanager.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @Path("run")
@@ -22,6 +24,17 @@ public class TestEndpoint {
 
         ProcessBuilder builder =  new ProcessBuilder("./run-tests.sh");
         Process process = builder.start();
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        while ( (line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(System.getProperty("line.separator"));
+        }
+        String result = stringBuilder.toString();
+        log.info(result);
 
         int exitCode = process.waitFor();
         assert exitCode == 0;
