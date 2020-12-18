7761c03c14168516c83e9c6e3ab05aa9971f034c
donnabauerc <donnabauer.christian.01@gmail.com>
2020-12-18 20:38:01 +0100

    return submtion

--- testing-api/src/main/java/at/htl/kafka/SubmitionListener.java
+++ testing-api/src/main/java/at/htl/kafka/SubmitionListener.java
@@ -1,5 +1,6 @@
 package at.htl.kafka;
 
+import at.htl.entities.LeocodeStatus;
 import at.htl.entities.Submition;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
@@ -13,8 +14,12 @@ public class SubmitionListener {
     @Inject
     Logger log;
 
+    @Inject
+    SubmitionProducer submitionProducer;
+
     @Incoming("submition-input")
     public void listen(Submition s) {
         log.info("Received Message: " + s.toString());
+        submitionProducer.sendSubmition(s);
     }
 }
---
+++ testing-api/src/main/java/at/htl/kafka/SubmitionProducer.java
@@ -0,0 +1,34 @@
+package at.htl.kafka;
+
+import at.htl.entities.LeocodeStatus;
+import at.htl.entities.Submition;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class SubmitionProducer {
+    @Inject
+    Logger log;
+
+    @Inject
+    @Channel("submition-result")
+    Emitter<Submition> emitter;
+
+    public void sendSubmition(Submition s) {
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(5000);
+                s.status = LeocodeStatus.SUCCESS;
+                emitter.send(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(runnable).start();
+        log.info("sent kafka message: " + s.toString());
+    }
+}
