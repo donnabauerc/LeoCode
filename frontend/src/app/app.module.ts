acbae42f9ffc28e66b0e3ef2f62fd8915bcf3361
donnabauerc <donnabauer.christian.01@gmail.com>
2021-01-23 11:41:46 +0100

    add routing and basic modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,11 +10,27 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ExampleListComponent } from './list-example/example-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import { CreateExampleComponent } from './create-example/create-example.component';
+import { TestExampleComponent } from './test-example/test-example.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+const appRoutes: Routes = [
+  {path: 'list-example', component: ExampleListComponent},
+  {path: 'create-example', component: CreateExampleComponent},
+  {path: 'test-example', component: TestExampleComponent},
+  {path: 'portfolio', component: PortfolioComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ExampleListComponent,
+    CreateExampleComponent,
+    TestExampleComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +40,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/test-example/test-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-example',
+  templateUrl: './test-example.component.html',
+  styleUrls: ['./test-example.component.css']
+})
+export class TestExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
