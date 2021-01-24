18b96e94c659a5c0fe1e8a872460737ab50ee925
donnabauerc <donnabauer.christian.01@gmail.com>
2021-01-24 22:40:20 +0100

    test examples

--- frontend/src/app/test-example/test-example.component.ts
+++ frontend/src/app/test-example/test-example.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpService} from '../services/http.service';
 
 @Component({
   selector: 'app-test-example',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestExampleComponent implements OnInit {
 
-  constructor() { }
+  exampleId: number;
+  form: FormData;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private http: HttpService) { }
 
   ngOnInit(): void {
+    this.checkPathParam();
+  }
+
+  checkPathParam(): boolean {
+    this.exampleId = +this.route.snapshot.paramMap.get('id');
+    if (0 !== this.exampleId) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  sendSubmission(): void {
+    this.form = new FormData(document.forms.namedItem('testExampleForm'));
+    if (this.checkPathParam()) {
+      this.form.set('example', String(this.exampleId));
+    }
+    this.http.testExample(this.form).subscribe(value => {
+      console.log(value);
+      // TODO: this.router.navigate(); to SSE
+    });
   }
 
 }
