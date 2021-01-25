aec15d10d23a4536777a3b99149ed5f5c76984ef
donnabauerc <donnabauer.christian.01@gmail.com>
2021-01-25 20:07:08 +0100

    change response for frontend

--- backend/src/main/java/at/htl/resources/PortfolioEndpoint.java
+++ backend/src/main/java/at/htl/resources/PortfolioEndpoint.java
@@ -1,15 +1,21 @@
 package at.htl.resources;
 
+import at.htl.entities.Submission;
 import at.htl.repositories.SubmissionRepository;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Path("/portfolio")
 public class PortfolioEndpoint {
@@ -25,6 +31,19 @@ public class PortfolioEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getFinishedSubmitions(@PathParam("username") String username){
         log.info("Received getFinishedSubmitions(" + username + ") Request");
-        return Response.ok(submissionRepository.getFinished(username)).build();
+        List<Submission> finishedSubmissions = submissionRepository.getFinished(username);
+        JsonArrayBuilder res = Json.createArrayBuilder();
+        finishedSubmissions.forEach(s -> {
+            res.add(Json.createObjectBuilder()
+                    .add("id", s.id)
+                    .add("status", s.getStatus().toString())
+                    .add("result", s.result)
+                    .add("lastTimeChanged", s.lastTimeChanged
+                            .format(DateTimeFormatter.ofPattern("HH:mm dd-MM-yyyy")))
+                    .build()
+            );
+        });
+
+        return Response.ok(res.build()).build();
     }
 }
